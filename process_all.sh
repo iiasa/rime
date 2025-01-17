16cd2efe7153f4d38de2751970b682d3f426e588
Mahé Perrette <mahe.perrette@gmail.com>
2025-01-17 09:21:24 +0000

    latest process_all.sh

--- process_all.sh
+++ process_all.sh
@@ -1,15 +1,15 @@
 names=(
-	# maize_yield
-	# rice_yield
-	# wheat_yield
-	# soy_yield
-	# daily_temperature_variability
-	# number_of_wet_days
-	# extreme_daily_rainfall
-	# rx5day
-	# wet_bulb_temperature
-	# river_discharge
-	# runoff
+	maize_yield
+	rice_yield
+	wheat_yield
+	soy_yield
+	daily_temperature_variability
+	number_of_wet_days
+	extreme_daily_rainfall
+	rx5day
+	wet_bulb_temperature
+	river_discharge
+	runoff
 	tas
 	pr
 	tasmax
@@ -23,17 +23,17 @@ names=(
 	rsds
 )
 
-# rime-download-isimip -v tas
-# rime-pre-gmt
-# rime-pre-wl
+rime-download-isimip -v tas
+rime-pre-gmt
+rime-pre-wl
 
 for name in ${names[@]}; do
 	echo ">>>$name<<<"
 	weight=latWeight
-	rime-download-isimip -i $name
-	rime-pre-region --weight $weight -i $name --cpus 20
-	rime-pre-quantilemap --weight $weight -i $name --regional-no-admin --regional --map --equi --map-chunk 90
+	# # weight=latWeight
+	rime-download-isimip -i $name --remove-daily
+	rime-pre-region --weight $weight -i $name --cpus 100
+	rime-pre-quantilemap --weight $weight -i $name --regional-no-admin --regional --equi --skip-nans --cpus 200
+	rime-pre-digitize -i $name --weights latWeight --cpus 200 --all-subregions
+	rime-pre-quantilemap --weight $weight -i $name --map --equi --map-chunk 180 --skip-nans --warming-levels 1.0 1.2 1.5 2.0 2.5 3.0 3.5 4.0 4.5 5.0 5.5 6.0 6.5 --quantile-bins 11
 done
-
-rime-pre-digitize -i ${names[@]} --weights latWeight
-rime-pre-digitize -i ${names[@]} --weights latWeight --all-subregions
